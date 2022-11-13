a910e1640e3135665e8c915e4a1d7c9a3619b216
Granat54 <ladukun.t.red@mail.ru>
2022-11-13 11:41:11 +0300

    task1

--- c#_lesson1/task1/Program.cs
+++ c#_lesson1/task1/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("ВВедите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int sqr = numberA * numberA;
+Console.WriteLine($"Квадрат числа {numberA} = {sqr}");
