8892f90be8dccb7a5e16a75cf473a041ab6a7400
Granat54 <ladukun.t.red@mail.ru>
2022-11-13 12:03:49 +0300

    task3

---
+++ c#_lesson1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите число соответствующее дню недели");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA == 1) System.Console.WriteLine("Понедельник");
+else if (numberA == 2) System.Console.WriteLine("Вторник");
+else if (numberA == 3) System.Console.WriteLine("Среда");
+else if (numberA == 4) System.Console.WriteLine("Четверг");
+else if (numberA == 5) System.Console.WriteLine("Пятница");
+else if (numberA == 6) System.Console.WriteLine("Суббота");
+else if (numberA == 7) System.Console.WriteLine("Воскресенье");
+else System.Console.WriteLine("Введено не корректное число, необходимо ввести число от 1 до 7");
