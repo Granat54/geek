c6f628eefd552143c30b5e3e31843017c28a957b
Granat54 <ladukun.t.red@mail.ru>
2022-11-13 11:55:51 +0300

    task2

---
+++ c#_lesson1/task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Проверка является ли первое число квадратом второго");
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB) System.Console.WriteLine("да");
+else System.Console.WriteLine("нет");
+   
