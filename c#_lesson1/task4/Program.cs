4f6257eb31022b93d34a76ec78c116705e82fac5
Granat54 <ladukun.t.red@mail.ru>
2022-11-13 12:24:38 +0300

    task4

---
+++ c#_lesson1/task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int b = number * (-1);
+while 
+    (b <= number){
+        Console.Write($"{b} ");
+        b = b + 1;
+    }
+
+   
