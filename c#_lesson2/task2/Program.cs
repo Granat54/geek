9d68079bf66b0ad4e0274328cda51b76865efdd7
Granat54 <ladukun.t.red@mail.ru>
2022-11-17 19:53:40 +0300

    lesson2

---
+++ c#_lesson2/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int RemoveSecond(int num)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+    int resuslt = firstDigit*10 + thirdDigit;
+    return resuslt;
+}
+int remove = RemoveSecond(number);
+Console.WriteLine(remove);
---
+++ c#_lesson2/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultiplicityTwoDigits (int num, int num1, int num2)
+{
+    return num % num1 == 0 && num % num2 == 0;
+}
+
+bool result = MultiplicityTwoDigits(number, 7, 23);
+
+string resultStr = result ? "Да" : "Нет";
+Console.WriteLine($" {number} -> {resultStr}");
