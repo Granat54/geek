92343056afbdc442d61c7d2e9d64bee467417747
Granat54 <ladukun.t.red@mail.ru>
2022-12-02 22:14:38 +0300

    Homework6

---
+++ c#_lesson6/Homework1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt (int size)
+{
+    int[] array = new int[size];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }        
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count+= 1;
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(numberM);
+int result = CountPositive(arr);
+PrintArray(arr);
+Console.Write($"-> {result}");
---
+++ c#_lesson6/Homework2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите число B1: ");
+double numberB1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B2: ");
+double numberB2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число K1: ");
+double numberK1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число K2: ");
+double numberK2 = Convert.ToInt32(Console.ReadLine());
+
+double FindX(double num1, double num2, double num3, double num4)
+{
+    double numX = (num1 - num2) / (num4 - num3);
+    return numX;
+}
+
+double FindY(double num1, double num2, double numX)
+{
+    double numY = num2*numX + num1;
+    return numY;
+}
+
+
+double numberX = FindX(numberB1, numberB2, numberK1, numberK2);
+double numberY = FindY(numberB1, numberK1, numberX);
+
+
+
+
+Console.Write($"b1 = {numberB1}, k1 = {numberK1}, b2 = {numberB2}, k2 = {numberK2} ");
+Console.WriteLine($"-> ({numberX} ; {numberY} )");
