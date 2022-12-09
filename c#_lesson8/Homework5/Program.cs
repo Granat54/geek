02a0beddc15c3c4ccd3eee1484e540fb22709729
Granat54 <ladukun.t.red@mail.ru>
2022-12-09 15:44:28 +0300

    Homework8

---
+++ c#_lesson8/Homework5/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    int i = 0;
+    int j = 0;
+
+    for (int x = 1; x <= matrix.GetLength(0) * matrix.GetLength(1); x++)
+    {
+        matrix[i, j] = x;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return matrix;
+}
+
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+int[,] matrix = CreateMatrix(4,4);
+PrintMatrix(matrix);
