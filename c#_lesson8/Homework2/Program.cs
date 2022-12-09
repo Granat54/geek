ffe228ebed517f1c30f8f9fc0d05a936f8ab2bd0
Granat54 <ladukun.t.red@mail.ru>
2022-12-09 15:25:44 +0300

    Homework8

---
+++ c#_lesson8/Homework2/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+int[] SumArray(int[,] matrix)
+{
+    int y = 0;
+    int[] arr = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i,j];
+            arr[y]= sum;
+        }
+        y++;
+    }
+    return arr;  
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void FindMin(int[] arr)
+{
+    int min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]<min) min = arr[i];
+    }
+    Console.WriteLine($"Строка с наименьшей суммой чисел -> {Array.IndexOf(arr, min)+1}");
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+int[] array = SumArray(array2D);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+FindMin(array);
+
+
---
+++ c#_lesson8/Homework3/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+
+int[,] MultipleMatrix(int[,] matrix, int[,] matrix1)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                newMatrix[i, j] += matrix[i, k] * matrix1[k, j];
+            }
+        }
+    }
+    return newMatrix;
+}
+
+
+
+int[,] array = CreateMatrixRndInt(3, 2, 1, 9);
+PrintMatrix(array);
+
+Console.WriteLine();
+
+int[,] array2D = CreateMatrixRndInt(2, 3, 1, 9);
+PrintMatrix(array2D);
+
+Console.WriteLine();
+
+if (array.GetLength(1) == array2D.GetLength(0)) 
+{
+    int[,] newArray = MultipleMatrix(array, array2D);
+    PrintMatrix(newArray);
+}
+else Console.WriteLine("Данные матрицы нельзя перемножить");
